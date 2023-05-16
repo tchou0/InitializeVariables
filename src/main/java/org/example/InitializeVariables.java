84d20e31bb6b70ca534bd9c112308af0f290f19f
Tou Chou <tchou@appdev.yearup.org>
2023-05-16 14:19:25 -0400

    updated

--- src/main/java/org/example/InitializeVariables.java
+++ src/main/java/org/example/InitializeVariables.java
@@ -2,18 +2,49 @@ package org.example;
 
 public class InitializeVariables {
 
+    /*
+     * There are three types of variables in Java:
+     *
+     *
+     * "local" or "automatic" variables declared in a function
+     * This is the heart of procedural coding
+     * All methods are separate
+     * You can use your local variables without thinking about other methods
+     *
+     *
+     * non-static "instance" variables that are replicated in
+     * each instance of a class
+     * This is the heart of OOP
+     * All objects are separate
+     * You can use your instance variables without thinking about other objects
+     *
+     *
+     * static "class" variables that are not replicated.
+     * A single variable is shared by all instances
+     * Useful for constants like Math.PI
+     * You can use your class variables without thinking about other classes
+     */
+
+
+//========= Initialization at point of declaration works for all three types ==========
     static int myStaticInt = 39;
+    // non-static "instance" variables have a lot of ways
+    // to initialize them!!!  %^}
+
+    // 1. initialization at point of declaration
     int myNonstaticInt = 14;
+
+    // 2. The non-static initializer block
     {
         myNonstaticInt = 24;
     }
 
-    // ============================ Constructor ==============================
+    // 3. ============================ Constructor ==============================
     public  InitializeVariables (int myNonstaticInt){
         this.myNonstaticInt = myNonstaticInt;
     }
 
-    //================== Overloaded Method 2nd Constructor ======================
+    //4. ================== Overloaded Method 2nd Constructor ======================
     public InitializeVariables(){
         myNonstaticInt = 352897;
     }
